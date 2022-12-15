04e5cb1f239829e6d4a8bacb7271ca74290809fb
Samad Khan <samadk619@gmail.com>
2022-12-15 11:42:08 +0530

    DB connection done

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express()
 const dotenv = require('dotenv')
+const mongoose = require("mongoose");
 dotenv.config()
+
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Mongo is Connected")
+  })
+  .catch((err) => {
+    if (err) {
+      console.log(`Failed to connect to MongoDB: ${err}`);
+    }
+  });
 
 app.use(express.json())
 // app.use('/api', require('./router.js'))
